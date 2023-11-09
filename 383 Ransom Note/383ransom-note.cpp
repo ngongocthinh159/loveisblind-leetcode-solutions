2064e0bae100dfe223cfd049e4727f72d2e4cc48
ngongocthinh124 <ngongocthinh124@gmail.com>
2023-11-09 21:59:01 +0000

    Code challenge submission for question: 383 Ransom Note

---
+++ 383 Ransom Note/383ransom-note.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> v(26,0);
+        for (int i = 0; i < magazine.size(); i++) {
+            v[magazine[i] - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.size(); i++) {
+            if (--v[ransomNote[i] - 'a'] == -1) return false;
+        }
+        return true;
+    }
+};
