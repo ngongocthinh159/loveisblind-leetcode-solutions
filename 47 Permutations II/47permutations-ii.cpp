124e6370f26179545d8bff82348de590b49de63b
ngongocthinh124 <ngongocthinh124@gmail.com>
2023-11-20 04:59:45 +0000

    Code challenge submission for question: 47 Permutations II

---
+++ 47 Permutations II/47permutations-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    /*
+        A key insight to avoid generating any redundant permutation (like permutation I problem) is that at each step rather than viewing each position (index) as a candidate, we consider each unique number as the true candidate.
+    */
+    vector<int> path;
+    vector<vector<int>> res;
+    unordered_map<int,int> mp;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+        dfs(nums);
+        return res;
+    }
+
+    void dfs(vector<int>& nums) {
+        if (path.size() == nums.size()) {
+            vector<int> tmp(path);
+            res.push_back(tmp);
+            return;
+        }
+
+        for (auto& [num, cnt] : mp) {
+            if (cnt == 0) continue;
+            cnt--;
+            path.push_back(num);
+
+            dfs(nums);
+
+            cnt++;
+            path.pop_back();
+        }
+    }
+    
+};
