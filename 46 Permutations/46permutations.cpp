d1c7de5f3ab945af7d6de6fdb81adb53d624134b
ngongocthinh124 <ngongocthinh124@gmail.com>
2023-11-20 05:17:30 +0000

    Code challenge submission for question: 46 Permutations

---
+++ 46 Permutations/46permutations.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /*
+        A key insight: for each index i, choose a possible index j with nums[j] to place in index i
+        Mark any choosen index j as visited
+    */
+    vector<int> path;
+    vector<vector<int>> res;
+    vector<bool> visited;
+    vector<vector<int>> permute(vector<int>& nums) {
+        visited = vector<bool>(nums.size(), false);
+        dfs(nums);
+        return res;
+    }
+
+    void dfs(vector<int>& nums) {
+        if (path.size() == nums.size()) {
+            vector<int> tmp(path);
+            res.push_back(tmp);
+            return;
+        }
+
+        // Pick an index j not yet visited (in the decision tree)
+        for (int j = 0; j < nums.size(); j++) {
+            if (!visited[j]) {
+                visited[j] = true;
+                path.push_back(nums[j]);
+                dfs(nums);
+                visited[j] = false;
+                path.pop_back();
+            }
+        }
+    }
+};
