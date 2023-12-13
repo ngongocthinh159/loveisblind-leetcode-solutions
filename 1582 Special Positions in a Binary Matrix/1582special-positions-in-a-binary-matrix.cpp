8a03860daa78cdadadef29f6558941f016a5467d
ngongocthinh124 <ngongocthinh124@gmail.com>
2023-12-13 03:50:23 +0000

    Code challenge submission for question: 1582 Special Positions in a Binary Matrix

---
+++ 1582 Special Positions in a Binary Matrix/1582special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        unordered_map<int,int> row;
+        unordered_map<int,int> col;
+        for (int i= 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[0].size(); j++) {
+                if (mat[i][j] == 1) {
+                    row[i]++; col[j]++;
+                }
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[0].size(); j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
