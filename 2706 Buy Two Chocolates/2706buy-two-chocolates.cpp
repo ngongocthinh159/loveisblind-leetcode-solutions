e1a61d283a91fd5ae1902847454e779e427ec5e9
ngongocthinh124 <ngongocthinh124@gmail.com>
2023-12-20 07:07:36 +0000

    Code challenge submission for question: 2706 Buy Two Chocolates

---
+++ 2706 Buy Two Chocolates/2706buy-two-chocolates.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int min1 = INT_MAX, min2 = INT_MAX;
+        for (auto price : prices) {
+            if (min1 > price) {
+                min2 = min1;
+                min1 = price;
+            } else if (min2 > price) {
+                min2 = price;
+            }
+        }
+        return (money - (min1 + min2) >= 0 ? money - (min1 + min2) : money);
+    }
+};
