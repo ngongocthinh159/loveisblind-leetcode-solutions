0075ea716db70e0d3a21c03b7f35b7d90d2a013b
ngongocthinh124 <ngongocthinh124@gmail.com>
2024-01-03 06:06:05 +0000

    Code challenge submission for question: 2125 Number of Laser Beams in a Bank

---
+++ 2125 Number of Laser Beams in a Bank/2125number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int prev = -1;
+        int ans = 0;
+        for (int i = 0; i < bank.size(); i++) {
+            int cnt = 0;
+            for (int j = 0; j < bank[0].size(); j++) {
+                if (bank[i][j] == '1') cnt++;
+            }
+            if (cnt == 0) continue;
+            if (prev == -1) {prev = cnt; continue;}
+            ans += cnt*prev;
+            prev = cnt;
+        }
+        return ans;
+    }
+};
