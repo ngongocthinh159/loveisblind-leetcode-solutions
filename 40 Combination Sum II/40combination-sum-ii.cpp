df869335fe6da9a536244481a1b318a829135e4a
ngongocthinh124 <ngongocthinh124@gmail.com>
2024-01-04 15:13:00 +0000

    Code challenge submission for question: 40 Combination Sum II

---
+++ 40 Combination Sum II/40combination-sum-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        unordered_map<int,int> jmp;
+        int i = 0; 
+        while (i < candidates.size()) {
+            int start = candidates[i];
+            while (i + 1 < candidates.size() && start == candidates[i + 1]) i++;
+            jmp[start] = i + 1;
+            i++;
+        }
+        dfs(0, target, candidates, jmp);
+        return res;
+    }
+    void dfs(int i, int target, vector<int> &candidates, unordered_map<int,int> &jmp) {
+        if (target == 0) {
+            res.push_back(path); return;
+        }
+        if (i >= candidates.size()) return;
+
+        // skip
+        dfs(jmp[candidates[i]], target, candidates, jmp);
+        
+        // choose
+        if (target >= candidates[i]) {
+            path.push_back(candidates[i]);
+            dfs(i + 1, target - candidates[i], candidates, jmp);
+            path.pop_back();
+        }
+    }
+};
