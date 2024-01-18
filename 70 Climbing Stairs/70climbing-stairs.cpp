afd30d0c82d54adc8d89f8996b64a0661938b122
ngongocthinh124 <ngongocthinh124@gmail.com>
2024-01-18 03:46:19 +0000

    Code challenge submission for question: 70 Climbing Stairs

---
+++ 70 Climbing Stairs/70climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + 1 <= n) dp[i] += dp[i + 1];
+            if (i + 2 <= n) dp[i] += dp[i + 2];
+        }
+        return dp[0];
+    }
+};
