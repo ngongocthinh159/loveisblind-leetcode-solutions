038b722bfd8b0db0ca52b468e1be432b8fd5c02e
ngongocthinh124 <ngongocthinh124@gmail.com>
2024-01-19 21:58:41 +0000

    Code challenge submission for question: 718 Maximum Length of Repeated Subarray

---
+++ 718 Maximum Length of Repeated Subarray/718maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,15 @@
+#define ll long long
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
+        int ans = INT_MIN;
+        for (int i = nums1.size() - 1; i >= 0; i--) {
+            for (int j = nums2.size() - 1; j >= 0; j--) {
+                if (nums1[i] == nums2[j]) dp[i][j] = 1 + dp[i + 1][j + 1];
+                ans = max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
