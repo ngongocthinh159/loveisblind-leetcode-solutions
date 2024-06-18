8cb476ede254edea382a676e3bb820ef18434217
ngongocthinh124 <ngongocthinh124@gmail.com>
2024-06-18 15:57:49 +0000

    Code challenge submission for question: 3185 Count Pairs That Form a Complete Day II

---
+++ 3185 Count Pairs That Form a Complete Day II/3185count-pairs-that-form-a-complete-day-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countCompleteDayPairs(vector<int>& hours) {
+        unordered_map<int,int> M;
+        int remain;
+        long long ans = 0;
+        for (int i = 0; i < hours.size(); i++) {
+            remain = hours[i] % 24;
+            if (M.count(24 - remain)) ans += M[24 - remain];
+            if (remain == 0 && M.count(0)) ans += M[0];
+            M[remain]++;
+        }
+        return ans;
+    }
+};
