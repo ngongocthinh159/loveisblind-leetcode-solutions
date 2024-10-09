3bc1e8b508155881a1131060b88556dc8f1697a5
ngongocthinh124 <ngongocthinh124@gmail.com>
2024-10-09 16:26:51 +0000

    Code challenge submission for question: 64 Minimum Path Sum

---
+++ 64 Minimum Path Sum/64minimum-path-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i - 1 >= 0) dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+                if (j - 1 >= 0) dp[i][j] = min(dp[i][j], dp[i][j - 1]);
+                if (dp[i][j] == INT_MAX) dp[i][j] = 0;
+                dp[i][j] += grid[i][j];
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
