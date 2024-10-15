8001b2fff12788738517c1a026321eb23ca34f96
ngongocthinh124 <ngongocthinh124@gmail.com>
2024-10-15 10:25:58 +0000

    Code challenge submission for question: 2938 Separate Black and White Balls

---
+++ 2938 Separate Black and White Balls/2938separate-black-and-white-balls.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long ans = 0;
+        int j = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '0')  {
+                ans += (i - j);
+                j++;
+            }
+        }
+        return ans;
+    }
+};
