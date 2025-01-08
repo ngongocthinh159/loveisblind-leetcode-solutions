f46d64b4cad92ffae0fff566197810fd5f240332
ngongocthinh124 <ngongocthinh124@gmail.com>
2025-01-08 09:43:55 +0000

    Code challenge submission for question: 3045 Count Prefix and Suffix Pairs II

---
+++ 3045 Count Prefix and Suffix Pairs II/3045count-prefix-and-suffix-pairs-ii.cpp
@@ -0,0 +1,60 @@
+struct Node {
+    vector<Node*> child;
+    Node* left;
+    Node* right;
+    bitset<100001> bs;
+    Node() {child.resize(26);}
+};
+
+class Solution {
+public:
+    long long countPrefixSuffixPairs(vector<string>& words) {
+        Node* pre_root = new Node;
+        Node* sur_root = new Node;
+        int n = words.size();
+        long long ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            Node* p = pre_root;
+            int idx;
+            bool found = true;
+            for (auto c : words[i]) {
+                idx = c - 'a';
+                if (!p->child[idx]) {
+                    found = false;
+                    break;
+                }
+                p = p->child[idx];
+            }
+            if (found) {
+                Node* p2 = sur_root;
+                bool found2 = true;
+                for (int j = words[i].size() - 1; j >= 0; j--) {
+                    idx = words[i][j] - 'a';
+                    if (!p2->child[idx]) {
+                        found2 = false;
+                        break;
+                    }
+                    p2 = p2->child[idx];
+                }
+                if (found2) {
+                    ans += (p->bs & p2->bs).count();
+                }
+            }
+            p = pre_root;
+            for (auto c : words[i]) {
+                idx = c - 'a';
+                if (!p->child[idx]) p->child[idx] = new Node;
+                p = p->child[idx];
+                p->bs.set(i);
+            }
+            p = sur_root;
+            for (int j = words[i].size() - 1; j>= 0; j--) {
+                idx = words[i][j] - 'a';
+                if (!p->child[idx]) p->child[idx] = new Node;
+                p = p->child[idx];
+                p->bs.set(i);
+            }
+        }
+        return ans;
+    }
+};
