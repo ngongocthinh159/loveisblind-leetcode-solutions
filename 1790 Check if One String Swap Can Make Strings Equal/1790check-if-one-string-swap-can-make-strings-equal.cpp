99d6d09fad8b10ed762319446254571639bb5241
ngongocthinh124 <ngongocthinh124@gmail.com>
2025-02-05 04:21:05 +0000

    Code challenge submission for question: 1790 Check if One String Swap Can Make Strings Equal

---
+++ 1790 Check if One String Swap Can Make Strings Equal/1790check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.size();
+        int idx1 = -1, idx2 = -1;
+        for (int i = 0; i < n; i++) {
+            if (s1[i]^s2[i]) {
+                if (idx1 == -1) idx1 = i;
+                else if (idx2 == -1) idx2 = i;
+                else return false;
+            }
+        }
+        if (idx1 == -1 && idx2 == -1) return true;
+        if (idx2 == -1) return false;
+        swap(s1[idx1], s1[idx2]);
+        return s1 == s2;
+    }
+};
