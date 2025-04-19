9983de565c32663c5d491738b03cb664ea93a0f4
ngongocthinh124 <ngongocthinh124@gmail.com>
2025-04-19 05:05:06 +0000

    Code challenge submission for question: 887 Super Egg Drop

---
+++ 887 Super Egg Drop/887super-egg-drop.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int superEggDrop(int k, int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1));
+        for (int i = 1; ; i++) {
+            for (int j = 1; j <= k; j++)
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1] + 1;
+            if (dp[i][k] >= n)
+                return i;
+        }
+        return -1;
+    }
+};
