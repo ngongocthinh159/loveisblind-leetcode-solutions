ae7eff9ef503db4324cc35bcff425890c4ac3984
ngongocthinh124 <ngongocthinh124@gmail.com>
2025-04-28 11:27:32 +0000

    Code challenge submission for question: 3130 Find All Possible Stable Binary Arrays II

---
+++ 3130 Find All Possible Stable Binary Arrays II/3130find-all-possible-stable-binary-arrays-ii.cpp
@@ -0,0 +1,49 @@
+#define N 1005
+int dp[N][N][2], pref[N][N][2], pref2[N][N][2];
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    void add(int &x, int y) {
+        x += y;
+        if (x >= MOD) x -= MOD;
+    }
+    void sub(int &x, int y) {
+        x -= y;
+        if (x < 0) x += MOD;
+    }
+    int numberOfStableArrays(int zero, int one, int limit) {
+        int n = zero + one;
+        for (int o = 0; o <= one + 2; o++)
+            for (int z = 0; z <= zero + 2; z++)
+                for (int j = 0; j < 2; j++)
+                    dp[o][z][j] = pref[o][z][j] = pref2[o][z][j] = 0;
+
+        for (int o = 0; o <= one; o++)
+            for (int z = 0; z <= zero; z++)
+                for (int j = 0; j < 2; j++) {
+                    if (o == 0 && z == 0) {
+                        dp[o][z][j] = 1;
+                    } else {
+                        if (j == 1) {
+                            int mx = min(limit, o);
+                            add(dp[o][z][j], pref[o][z][0]);
+                            sub(dp[o][z][j], pref[o - mx][z][0]);
+                        }
+                        else {
+                            int mx = min(limit, z);
+                            add(dp[o][z][j], pref2[o][z][1]);
+                            sub(dp[o][z][j], pref2[o][z - mx][1]);
+                        }
+                    }
+
+                    pref[o + 1][z][j] = pref[o][z][j];
+                    add(pref[o + 1][z][j], dp[o][z][j]);
+                    pref2[o][z + 1][j] = pref2[o][z][j];
+                    add(pref2[o][z + 1][j], dp[o][z][j]);
+                }
+        int ans = 0;
+        add(ans, dp[one][zero][0]);
+        add(ans, dp[one][zero][1]);
+        return ans;
+    }
+};
