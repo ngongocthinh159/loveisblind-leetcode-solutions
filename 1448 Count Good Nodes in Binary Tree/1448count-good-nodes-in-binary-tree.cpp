9e648b656e37318eceb5592e327ad65a14d22e2f
ngongocthinh124 <ngongocthinh124@gmail.com>
2025-07-15 15:45:48 +0000

    Code challenge submission for question: 1448 Count Good Nodes in Binary Tree

---
+++ 1448 Count Good Nodes in Binary Tree/1448count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, int mx, int &cnt) {
+        if (!root) return;
+        if (mx<=root->val) cnt++;
+        dfs(root->left, max(mx, root->val), cnt);
+        dfs(root->right, max(mx, root->val), cnt);
+    }
+    int goodNodes(TreeNode* root) {
+        int cnt = 0;
+        dfs(root, -1e5, cnt);
+        return cnt;
+    }
+};
